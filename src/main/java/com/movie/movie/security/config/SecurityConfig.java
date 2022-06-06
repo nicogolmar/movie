0d55bb8a06428d15df4132c1a33335baf1db05f4
nicogolmar <nico.bocakapo09@gmail.com>
2022-06-05 23:39:01 -0300

    Spring Movies - FRONT CONECTION TRUE2

--- src/main/java/com/movie/movie/security/config/SecurityConfig.java
+++ src/main/java/com/movie/movie/security/config/SecurityConfig.java
@@ -83,7 +83,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().antMatchers("/api/auth/**").permitAll()
         //Permisos a diferentes path
-                .antMatchers("/api/movies").permitAll()
+                .antMatchers("/home").permitAll()
                .antMatchers("/api/movieId/**").permitAll()
                 .anyRequest().authenticated();
 
